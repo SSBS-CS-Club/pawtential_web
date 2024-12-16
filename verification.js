9e23bb3928c6941da6dfcac0791d46d27cbb671d
George 施享 <GeorgeShiThy@Outlook.com>
2024-12-16 22:09:14 +0800

    auth partly added

---
+++ verification.js
@@ -0,0 +1,15 @@
+// Get the email field's reference 
+const emailField = document.getElementById("email");
+
+// Get the password field's reference
+const passwordField = document.getElementById("password");
+
+// Sign in button
+const signInButton = document.getElementById("signIn");
+
+const imaginaryDatabase = [
+    {
+        email: "",
+        password: "",
+    },
+];
